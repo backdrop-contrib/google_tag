f57c61e6d27468563a26be25bbae24e75a909a32
Kevin Thull <kthull@bluedropshop.com>
2019-08-15 11:48:01 -0500

    convert js file

--- js/google_tag.admin.js
+++ js/google_tag.admin.js
@@ -10,34 +10,34 @@
 /**
  * Provides summary information for the vertical tabs.
  */
-Drupal.behaviors.gtmInsertionSettings = {
+Backdrop.behaviors.gtmInsertionSettings = {
   attach: function (context) {
-    if (typeof jQuery.fn.drupalSetSummary == 'undefined') {
-      // This behavior only applies if drupalSetSummary is defined.
+    if (typeof jQuery.fn.backdropSetSummary == 'undefined') {
+      // This behavior only applies if backdropSetSummary is defined.
       return;
     }
 
-    $('fieldset#edit-path', context).drupalSetSummary(function (context) {
+    $('fieldset#edit-path', context).backdropSetSummary(function (context) {
       var $radio = $('input[name="google_tag_path_toggle"]:checked', context);
       if ($radio.val() == 'exclude listed') {
         if (!$('textarea[name="google_tag_path_list"]', context).val()) {
-          return Drupal.t('All paths');
+          return Backdrop.t('All paths');
         }
         else {
-          return Drupal.t('All paths except listed paths');
+          return Backdrop.t('All paths except listed paths');
         }
       }
       else {
         if (!$('textarea[name="google_tag_path_list"]', context).val()) {
-          return Drupal.t('No paths');
+          return Backdrop.t('No paths');
         }
         else {
-          return Drupal.t('Only listed paths');
+          return Backdrop.t('Only listed paths');
         }
       }
     });
 
-    $('fieldset#edit-role', context).drupalSetSummary(function (context) {
+    $('fieldset#edit-role', context).backdropSetSummary(function (context) {
       var vals = [];
       $('input[type="checkbox"]:checked', context).each(function () {
         vals.push($.trim($(this).next('label').text()));
@@ -45,38 +45,38 @@ Drupal.behaviors.gtmInsertionSettings = {
       var $radio = $('input[name="google_tag_role_toggle"]:checked', context);
       if ($radio.val() == 'exclude listed') {
         if (!vals.length) {
-          return Drupal.t('All roles');
+          return Backdrop.t('All roles');
         }
         else {
-          return Drupal.t('All roles except selected roles');
+          return Backdrop.t('All roles except selected roles');
         }
       }
       else {
         if (!vals.length) {
-          return Drupal.t('No roles');
+          return Backdrop.t('No roles');
         }
         else {
-          return Drupal.t('Only selected roles');
+          return Backdrop.t('Only selected roles');
         }
       }
     });
 
-    $('fieldset#edit-status', context).drupalSetSummary(function (context) {
+    $('fieldset#edit-status', context).backdropSetSummary(function (context) {
       var $radio = $('input[name="google_tag_status_toggle"]:checked', context);
       if ($radio.val() == 'exclude listed') {
         if (!$('textarea[name="google_tag_status_list"]', context).val()) {
-          return Drupal.t('All statuses');
+          return Backdrop.t('All statuses');
         }
         else {
-          return Drupal.t('All statuses except listed statuses');
+          return Backdrop.t('All statuses except listed statuses');
         }
       }
       else {
         if (!$('textarea[name="google_tag_status_list"]', context).val()) {
-          return Drupal.t('No statuses');
+          return Backdrop.t('No statuses');
         }
         else {
-          return Drupal.t('Only listed statuses');
+          return Backdrop.t('Only listed statuses');
         }
       }
     });
